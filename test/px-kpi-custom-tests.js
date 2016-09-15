9f504dcd08265880ed1d6d1bdf4fe4c6920005af
Randy Askin <randall.askin@ge.com>
2016-09-15 10:28:32 -0700

    Added unit tests

--- test/px-kpi-custom-tests.js
+++ test/px-kpi-custom-tests.js
@@ -1,10 +1,33 @@
 // This is the wrapper for custom tests, called upon web components ready state
 function runCustomTests() {
-  // Place any setup steps like variable declaration and initialization here
 
-  // This is the placeholder suite to place custom tests in
-  // Use testCase(options) for a more convenient setup of the test cases
-  suite('Custom Automation Tests for px-kpi', function() {
-    // TODO: write unit tests for px-kpi
+  suite('Testing of the main kpi component', function() {
+    test('KPI is created', function() {
+      var kpi = document.querySelector('px-kpi');
+      assert.isTrue(kpi !== undefined);
+    });
+    test('KPI title is displayed', function() {
+      var kpi = document.querySelector('px-kpi'),
+          title = kpi.querySelector('h3').textContent;
+      assert.equal(title, 'Test');
+    });
+    test('Status icon is displayed correctly', function() {
+      var kpi = document.querySelector('px-kpi'),
+          icon = kpi.querySelector('#statusIcon');
+      assert.isTrue(icon !== undefined);
+      assert.equal(icon.style.color, 'green');
+    });
+  });
+
+  suite('Testing of the spark component', function() {
+    test('Spark is created', function() {
+      var spark = document.querySelector('px-vis-spark');
+      assert.isTrue(spark !== undefined);
+    });
+    test('Spark chart is proper size', function() {
+      var chart = document.querySelector('px-simple-win-loss-chart'),
+          width = window.getComputedStyle(chart).width;
+      assert.equal(width, '300px');
+    });
   });
 };
