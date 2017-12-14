6149830fc80d7332dca383f527e5e8cabd92d559
Martin Wragg <mdwragg@gmail.com>
2017-12-14 13:42:03 -0800

    Fix up tests.

--- test/px-kpi-custom-tests.js
+++ test/px-kpi-custom-tests.js
@@ -1,36 +1,45 @@
 // This is the wrapper for custom tests, called upon web components ready state
-function runCustomTests() {
 
-  suite('Testing of the main kpi component', function() {
-    test('KPI is created', function() {
-      var kpi = document.querySelector('px-kpi');
-      assert.isTrue(kpi !== undefined);
-    });
-    test('KPI title is displayed', function() {
-      var kpi = document.querySelector('px-kpi'),
-          title = Polymer.dom(kpi.root).querySelector('span').textContent;
-
-      assert.equal(title, 'Test');
-    });
-    test('Status icon is displayed correctly', function() {
-      var kpi = document.querySelector('px-kpi'),
-          icon = kpi.querySelector('#statusIcon');
-      assert.isTrue(icon !== undefined);
-      assert.equal(icon.style.color, 'green');
-    });
+suite('Testing of the main kpi component', done => {
+  let kpi;
+  setup(done => {
+    kpi = fixture('px_kpi_1');
+    flush(() => done());
   });
 
-  suite('Testing of the spark component', function() {
-    test('Spark is created', function() {
-      var spark = document.querySelector('px-vis-spark');
-      assert.isTrue(spark !== undefined);
-    });
-    test('Spark chart is proper size', function() {
-      var chart = document.querySelector('px-simple-win-loss-chart'),
-          width = window.getComputedStyle(chart).width;
-      flush(function(){
-        assert.equal(width, '300px');
-      });
+  test('KPI is created', function() {
+    assert.isTrue(kpi !== undefined);
+  });
+  test('KPI title is displayed', function() {
+    let title = Polymer.dom(kpi.root).querySelector('span').textContent;
+    assert.equal(title, 'Test');
+  });
+  test('Status icon is displayed correctly', function() {
+    let icon = Polymer.dom(kpi.root).querySelector('#statusIcon');
+    assert.isTrue(icon !== undefined);
+    assert.equal(icon.style.color, 'green');
+  });
+});
+
+suite('Testing of the spark component', function() {
+  let kpi;
+  setup(done => {
+    kpi = fixture('px_kpi_1');
+    flush(() => done());
+  });
+
+  test('Spark is created', function() {
+    let spark = Polymer.dom(kpi.root).querySelector('px-vis-spark');
+    assert.isTrue(spark !== undefined);
+  });
+
+  test('Spark chart is proper size', done => {
+    let spark = Polymer.dom(kpi.root).querySelector('px-vis-spark'),
+        width = window.getComputedStyle(spark).width;
+
+    flush(function(){
+      assert.equal(width, 'auto');
+      done();
     });
   });
-}
+});
